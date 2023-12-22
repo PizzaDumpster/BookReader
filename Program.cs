785a5fa9ce371fda70c7843fc5088ad08f203112
PizzaDumpster <djd187@hotmail.com>
2023-12-21 22:46:37 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Speech.Synthesis;
+
+namespace BookReader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialize a new instance of the SpeechSynthesizer.
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+
+            // Configure the audio output.
+            synth.SetOutputToDefaultAudioDevice();
+
+            synth.Volume = 50;
+
+            // Speak a string.
+            foreach (var myString in File.ReadAllLines(@"C:\Users\djd18\OneDrive\Desktop\pg46.txt"))
+            {
+                Console.WriteLine(myString);
+                synth.Speak(myString);
+                
+            }
+                
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
